b8d14782455e568137a00bed327cddf61f34f5e5
Raul Fernando <raulfernando08@gmail.com>
2017-09-01 17:51:58 -0300

    final

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -803,7 +803,9 @@ References:
 })(this);
 function openNav() {
   document.querySelector(".menu-responsivo").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
 }
 function closeNav() {
   document.querySelector(".menu-responsivo").style.width = "0";
+  document.body.style.backgroundColor = "white";
 }
--- app/scripts/src/main.js
+++ app/scripts/src/main.js
@@ -1,6 +1,8 @@
 function openNav() {
   document.querySelector(".menu-responsivo").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
 }
 function closeNav() {
   document.querySelector(".menu-responsivo").style.width = "0";
+  document.body.style.backgroundColor = "white";
 }
